cec32c8daf3af6ea7a77e7086b4d9e27c4311c8c
loop <1477824757@qq.com>
2023-07-06 10:58:05 +0800

    自动返回json

--- SpringBoot/RestfulSwagger/src/main/java/com/loop/controller/UserController.java
+++ SpringBoot/RestfulSwagger/src/main/java/com/loop/controller/UserController.java
@@ -6,10 +6,7 @@ import com.loop.pojo.User;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * <p>
@@ -21,15 +18,23 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @Api(tags = "测试！")
 @RestController
+@CrossOrigin  // 当前所有API允许跨域访问
 @RequestMapping("/user")
 public class UserController {
     @Autowired
     UserMapper userMapper;
     @ApiOperation("测试读取一条记录！")
-    @GetMapping
+    @GetMapping("/1")
     public String test1(){
         User user = userMapper.selectById(1L);
         return user.toString();
+    }
+    @ApiOperation("测试读取一条记录返回json！")
+    @ResponseBody //自动返回json数据
+    @GetMapping("/2")
+    public User test2(){
+        User user = userMapper.selectById(1L);
+        return user;
     }
 }
 
